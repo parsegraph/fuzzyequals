567bd3e580792105393d745bf02307e07c984220
Aaron Faanes <dafrito@gmail.com>
2020-12-06 03:56:09 -0600

    Implement package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export default function getTimeInMillis() {
-  return new Date().getTime();
+// eslint-disable-next-line require-jsdoc
+export default function fuzzyEquals(a, b, fuzziness) {
+  if (!fuzziness) {
+    return (isNaN(a) && isNaN(b)) || a === b;
+  }
+  if (a < b) {
+    return Math.abs(b / a) - 1 <= fuzziness;
+  }
+  if (Math.abs(b) <= fuzziness) {
+    return a - b <= fuzziness;
+  }
+  return Math.abs(a / b) - 1 <= fuzziness;
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 var assert = require("assert");
-import getTimeInMillis from "../dist/gettimeinmillis";
+import fuzzyEquals from "../dist/fuzzyequals";
 
-describe("getTimeInMillis", function () {
-  it("works", ()=>{
-    assert.ok(!isNaN(getTimeInMillis()));
+describe("fuzzyEquals", function () {
+  it("treats equal numbers as equal", ()=>{
+    assert.equal(fuzzyEquals(1, 1, .1), true);
+  });
+  it("treats close numbers as equal", ()=>{
+    assert.equal(fuzzyEquals(1, 1.0000001, .1), true);
+  });
+  it("treats grossly unequal numbers as unequal", ()=>{
+    assert.ok(!fuzzyEquals(1, 2.0000001, .1));
   });
 });
