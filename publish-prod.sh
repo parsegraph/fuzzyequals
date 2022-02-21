a2e1490d21447e6808af79bfe207303988cfeaf8
Aaron Faanes <dafrito@gmail.com>
2022-02-21 13:13:38 -0600

    upgrade-microproject fixes

--- publish-prod.sh
+++ publish-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PUBLISH="npm pub parsegraph-fuzzyequals-dev.tgz --tag=dev"
+PUBLISH="npm pub parsegraph-fuzzyequals-prod.tgz"
 $PUBLISH && exit
 for try in `seq 1 3`; do
     sleep $try
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 export class Fuzziness {
-  _fuzziness:number;
+  _fuzziness: number;
 
-  constructor(value:number) {
+  constructor(value: number) {
     this.setFuzziness(value);
   }
 
-  setFuzziness(value:number) {
+  setFuzziness(value: number) {
     this._fuzziness = value;
   }
 
-  getFuzziness():number {
-    return this._fuzziness !== undefined ?
-      this._fuzziness : defaultFuzziness.getFuzziness();
+  getFuzziness(): number {
+    return this._fuzziness !== undefined
+      ? this._fuzziness
+      : defaultFuzziness.getFuzziness();
   }
 
   resetFuzziness() {
     this.setFuzziness(undefined);
   }
 
-  check(a:number, b:number):boolean {
+  check(a: number, b: number): boolean {
     const fuzziness = this.getFuzziness();
     if (!fuzziness) {
       return a === b;
@@ -28,7 +29,7 @@ export class Fuzziness {
 }
 
 const defaultFuzziness = new Fuzziness(1e-6);
-export function setFuzziness(value:number) {
+export function setFuzziness(value: number) {
   defaultFuzziness.setFuzziness(value);
 }
 
@@ -40,7 +41,7 @@ export function cloneFuzziness() {
   return new Fuzziness(defaultFuzziness.getFuzziness());
 }
 
-export default function fuzzyEquals(a:number, b:number, fuzziness?:number) {
+export default function fuzzyEquals(a: number, b: number, fuzziness?: number) {
   let calc = defaultFuzziness;
   if (fuzziness !== undefined) {
     calc = new Fuzziness(fuzziness);
