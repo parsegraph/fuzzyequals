122dd64e4af04f1801ef85bcda613c629ef40ab4
Aaron Faanes <dafrito@gmail.com>
2022-02-21 13:30:00 -0600

    Print fuzziness in demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,11 +1,11 @@
-import todo from ".";
+import { getFuzziness } from ".";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
   root.style.position = "relative";
 
   const container = document.createElement("div");
-  container.innerHTML = `${todo(2, 2.01)}`;
+  container.innerHTML = `${getFuzziness()}`;
   container.style.position = "absolute";
   container.style.left = "0px";
   container.style.top = "0px";
